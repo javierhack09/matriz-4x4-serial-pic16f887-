8aded5fb5838c4905fe3db421f175904a7baf1d9
javierhack09 <72482239+javierhack09@users.noreply.github.com>
2022-11-22 10:33:44 -0500

    Add files via upload

---
+++ WindowsFormsApplication8/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace WindowsFormsApplication8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.ON = new System.Windows.Forms.Button();
+            this.OFF = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.clear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM8";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // ON
+            // 
+            this.ON.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ON.Location = new System.Drawing.Point(222, 43);
+            this.ON.Name = "ON";
+            this.ON.Size = new System.Drawing.Size(75, 23);
+            this.ON.TabIndex = 0;
+            this.ON.Text = "ON";
+            this.ON.UseVisualStyleBackColor = false;
+            this.ON.Click += new System.EventHandler(this.ON_Click);
+            // 
+            // OFF
+            // 
+            this.OFF.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.OFF.Location = new System.Drawing.Point(449, 43);
+            this.OFF.Name = "OFF";
+            this.OFF.Size = new System.Drawing.Size(75, 23);
+            this.OFF.TabIndex = 1;
+            this.OFF.Text = "OFF";
+            this.OFF.UseVisualStyleBackColor = false;
+            this.OFF.Click += new System.EventHandler(this.OFF_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(565, 189);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(161, 108);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox4.TabIndex = 6;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(382, 92);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(142, 108);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(217, 189);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(138, 108);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(35, 82);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(153, 108);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(771, 117);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(152, 108);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox5.TabIndex = 11;
+            this.pictureBox5.TabStop = false;
+            // 
+            // clear
+            // 
+            this.clear.BackColor = System.Drawing.Color.DodgerBlue;
+            this.clear.Location = new System.Drawing.Point(674, 43);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(75, 23);
+            this.clear.TabIndex = 12;
+            this.clear.Text = "CLEAR";
+            this.clear.UseVisualStyleBackColor = false;
+            this.clear.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 200.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(796, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 302);
+            this.label1.TabIndex = 13;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.pictureBox6.Image = global::WindowsFormsApplication8.Properties.Resources.FONDOPANTALLA;
+            this.pictureBox6.Location = new System.Drawing.Point(0, 1);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(1153, 434);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 14;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1165, 437);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.OFF);
+            this.Controls.Add(this.ON);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button ON;
+        private System.Windows.Forms.Button OFF;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox6;
+    }
+}
+
---
+++ WindowsFormsApplication8/Form1.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication8
+{
+    public partial class Form1 : Form
+    {
+        int flag = 0,i=0;
+        string lectura;
+        float numero1 = 0,numero2=0,total=0;
+        string[] lectura2 = new string[10];
+        
+        public Form1()
+        {
+            InitializeComponent();
+            ON.Enabled = true;
+            OFF.Enabled = false;
+            try { serialPort1.Open(); }
+            catch (Exception msg) { MessageBox.Show(msg.ToString()); }
+            
+        }
+        
+        private void ON_Click(object sender, EventArgs e)
+        {
+            flag = 1;
+            ON.Enabled = false;
+            OFF.Enabled = true;
+            clear.Enabled = true;
+            pictureBox4.Visible = true;
+            pictureBox3.Visible = true;
+            pictureBox2.Visible = true;
+            pictureBox1.Visible = true;
+        }
+
+        private void OFF_Click(object sender, EventArgs e)
+        {
+            flag = 2;
+            OFF.Enabled = false;
+            ON.Enabled = true;
+            clear.Enabled = false;
+            pictureBox4.Visible = false;
+            pictureBox3.Visible = false;
+            pictureBox2.Visible = false;
+            pictureBox1.Visible = false;
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            if(flag == 1)
+            {
+                lectura = serialPort1.ReadLine();
+                
+                lectura2[i] = lectura;
+
+                i++;
+
+                if(i>=4)
+                {
+                    i = 0;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lectura2[0] = "";
+            lectura2[1] = "";
+            lectura2[2] = "";
+            lectura2[3] = "";
+            label1.Text = "";
+            pictureBox1.Image = WindowsFormsApplication8.Properties.Resources.blanco;
+            pictureBox2.Image = WindowsFormsApplication8.Properties.Resources.blanco;
+            pictureBox3.Image = WindowsFormsApplication8.Properties.Resources.blanco;
+            pictureBox4.Image = WindowsFormsApplication8.Properties.Resources.blanco;
+            pictureBox5.Image = null;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            
+            if (lectura2[0] == "1")
+            {
+                pictureBox1.Image = WindowsFormsApplication8.Properties.Resources.uno;
+                numero1 = 1;
+            }
+
+            if (lectura2[0] == "2")
+            {
+                pictureBox1.Image = WindowsFormsApplication8.Properties.Resources.dos;
+                numero1 = 2;
+            }
+            if (lectura2[0] == "3")
+            {
+                pictureBox1.Image = WindowsFormsApplication8.Properties.Resources.tres;
+                numero1 = 3;
+            }
+            if (lectura2[0] == "4")
+            {
+                pictureBox1.Image = WindowsFormsApplication8.Properties.Resources.cuatro;
+                numero1 = 4;
+            }
+            if (lectura2[0] == "5")
+            {
+                pictureBox1.Image = WindowsFormsApplication8.Properties.Resources.cinco;
+                numero1 = 5;
+            }
+
+            if (lectura2[0] == "6")
+            {
+                pictureBox1.Image = WindowsFormsApplication8.Properties.Resources.seis;
+                numero1 = 6;
+            }
+
+            if (lectura2[0] == "7")
+            {
+                pictureBox1.Image = WindowsFormsApplication8.Properties.Resources.siete;
+                numero1 = 7;
+            }
+
+            if (lectura2[0] == "8")
+            {
+                pictureBox1.Image = WindowsFormsApplication8.Properties.Resources.ocho;
+                numero1 = 8;
+            }
+
+            if (lectura2[0] == "9")
+            {
+                pictureBox1.Image = WindowsFormsApplication8.Properties.Resources.nueve;
+                numero1 = 9;
+            }
+            if (lectura2[0] == "0")
+            {
+                pictureBox1.Image = WindowsFormsApplication8.Properties.Resources.cero;
+                numero1 = 0;
+            }
+
+            if (lectura2[1] == "A")
+            {
+                pictureBox2.Image = WindowsFormsApplication8.Properties.Resources.mas1;
+                total = numero1 + numero2;
+                if (lectura2[3] == "#")
+                {
+                   
+                    pictureBox4.Image = WindowsFormsApplication8.Properties.Resources.igual3;
+                    string strImage = total.ToString();
+                    //CreateImageFromString(strImage);
+                    //pictureBox5.Image = Image.FromFile("C:\\AMD\\prueba.png");
+                    label1.Text = total.ToString();
+                    lectura2[3] = "*";
+                }
+            }
+            if (lectura2[1] == "B")
+            {
+                pictureBox2.Image = WindowsFormsApplication8.Properties.Resources.menos1;
+                total = numero1 - numero2;
+                if (lectura2[3] == "#")
+                {
+                    
+                    pictureBox4.Image = WindowsFormsApplication8.Properties.Resources.igual3;
+                    string strImage = total.ToString();
+                    //CreateImageFromString(strImage);
+                    //pictureBox5.Image = Image.FromFile("C:\\AMD\\prueba.png");
+                    label1.Text = total.ToString();
+                    lectura2[3] = "*";
+                }
+            }
+            if (lectura2[1] == "C")
+            {
+                pictureBox2.Image = WindowsFormsApplication8.Properties.Resources.por1;
+                total = numero1 * numero2;
+                if (lectura2[3] == "#")
+                {
+                    
+                    pictureBox4.Image = WindowsFormsApplication8.Properties.Resources.igual3;
+                    string strImage = total.ToString();
+                    //CreateImageFromString(strImage);
+                    //pictureBox5.Image = Image.FromFile("C:\\AMD\\prueba.png");
+                    label1.Text = total.ToString();
+                    lectura2[3] = "*";
+                }
+            }
+            if (lectura2[1] == "D")
+            {
+                pictureBox2.Image = WindowsFormsApplication8.Properties.Resources.division1;
+                total = numero1 / numero2;
+                if (lectura2[3] == "#")
+                {
+                    
+                    pictureBox4.Image = WindowsFormsApplication8.Properties.Resources.igual3;
+                    string strImage = total.ToString();
+                    //CreateImageFromString(strImage);
+                    //pictureBox5.Image = Image.FromFile("C:\\AMD\\prueba.png");
+                    label1.Text = total.ToString();
+                    lectura2[3] = "*";
+                    
+
+                }
+            }
+
+            if (lectura2[2] == "1")
+            {
+                pictureBox3.Image = WindowsFormsApplication8.Properties.Resources.uno;
+                numero2 = 1;
+            }
+
+            if (lectura2[2] == "2")
+            {
+                pictureBox3.Image = WindowsFormsApplication8.Properties.Resources.dos;
+                numero2 = 2;
+            }
+            if (lectura2[2] == "3")
+            {
+                pictureBox3.Image = WindowsFormsApplication8.Properties.Resources.tres;
+                numero2 = 3;
+            }
+            if (lectura2[2] == "4")
+            {
+                pictureBox3.Image = WindowsFormsApplication8.Properties.Resources.cuatro;
+                numero2 = 4;
+            }
+            if (lectura2[2] == "5")
+            {
+                pictureBox3.Image = WindowsFormsApplication8.Properties.Resources.cinco;
+                numero2 = 5;
+            }
+
+            if (lectura2[2] == "6")
+            {
+                pictureBox3.Image = WindowsFormsApplication8.Properties.Resources.seis;
+                numero2 = 6;
+            }
+
+            if (lectura2[2] == "7")
+            {
+                pictureBox3.Image = WindowsFormsApplication8.Properties.Resources.siete;
+                numero2 = 7;
+            }
+
+            if (lectura2[2] == "8")
+            {
+                pictureBox3.Image = WindowsFormsApplication8.Properties.Resources.ocho;
+                numero2 = 8;
+            }
+
+            if (lectura2[2] == "9")
+            {
+                pictureBox3.Image = WindowsFormsApplication8.Properties.Resources.nueve;
+                numero2 = 9;
+            }
+            if (lectura2[2] == "0")
+            {
+                pictureBox3.Image = WindowsFormsApplication8.Properties.Resources.cero;
+                numero2 = 0;
+            }
+
+
+        }
+
+       
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        public void CreateImageFromString(string strImage)
+        {
+
+            string text = strImage;
+            //create new image
+            Bitmap bitmap = new Bitmap(1, 1);
+            //Properties string to draw
+            Font font = new Font("Arial", 200, FontStyle.Regular, GraphicsUnit.Pixel);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            //properties new image
+            int width = (int)graphics.MeasureString(text, font).Width;
+            int height = (int)graphics.MeasureString(text, font).Height;
+            bitmap = new Bitmap(bitmap, new Size(width, height));
+            //add text to image
+            graphics = Graphics.FromImage(bitmap);
+            graphics.Clear(Color.White);
+            //graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            //graphics.TextRenderingHint = TextRenderingHint.AntiAlias;
+            graphics.DrawString(text, font, new SolidBrush(Color.Black), 0, 0);
+            //execute pending graphics
+            graphics.Flush();
+            //release resources used by graphics
+            graphics.Dispose();
+            //save the image
+            bitmap.Save("C:\\AMD\\prueba.png", System.Drawing.Imaging.ImageFormat.Png);
+            //do something with image
+            
+        }
+
+
+
+
+    }
+}
